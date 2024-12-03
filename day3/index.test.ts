69e82ab9825fca4e9ab33875a905377f4f16d3c3
RTongue <r.tongue@gmail.com>
2024-12-03 00:33:13 -0500

    Day 3 solution

--- day3/index.test.ts
+++ day3/index.test.ts
@@ -3,15 +3,15 @@ import { solvePartOne, solvePartTwo } from './solve'
 describe('Day 3', () => {
   describe('part one', () => {
     it('solves for example input', () => {
-      const answer = solvePartOne(``)
-      expect(answer).toEqual(0)
+      const answer = solvePartOne(`xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))`)
+      expect(answer).toEqual(161)
     })
   })
 
   describe('part two', () => {
     it('solves for example input', () => {
-      const answer = solvePartTwo(``)
-      expect(answer).toEqual(0)
+      const answer = solvePartTwo(`xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))`)
+      expect(answer).toEqual(48)
     })
   })
 })
--- day3/index.ts
+++ day3/index.ts
@@ -3,8 +3,8 @@ import { getInput } from '../utils/getInput'
 
 getInput('https://adventofcode.com/2024/day/3/input')
   .then((res: string) => {
-    const answerOne = solvePartOne(res)
-    console.log('Day 3 - Answer one', answerOne)
+    // const answerOne = solvePartOne(res)
+    // console.log('Day 3 - Answer one', answerOne)
 
     const answerTwo = solvePartTwo(res)
     console.log('Day 3 - Answer two', answerTwo)
