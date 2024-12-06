c429d6fd93e991a1685ffe8f119c8719139b6998
Robbyn Tongue <r.tongue@gmail.com>
2024-12-06 07:13:23 -0500

    Day 6 WIP

--- day6/solve.ts
+++ day6/solve.ts
@@ -1,5 +1,32 @@
+import { twoDimensionalArray } from '../utils/twoDimensionalArray'
+
+const directions = {
+  '^': [-1, 0],
+  '>': [0, 1],
+  
+}
+
 export function solvePartOne (input: string) {
   // Code to solve part one here
+  const grid: string[][] = twoDimensionalArray(input, '\n', '')
+  const [startX, startY] = grid.reduce((accum, row, x) => {
+    const y = row.reduce((accum, cell, y) => {
+      if (cell !== '.' && cell !== '#') {
+        return y
+      }
+      return accum
+    }, null)
+
+    if (y !== null) {
+      return [x, y]
+    }
+
+    return accum
+  }, [null, null])
+
+  console.log('[x, y', [startX, startY])
+  console.log('Start', grid[startX][startY])
+
   return 0
 }
 
